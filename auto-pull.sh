04aa0b07740a46d7ee096c7e1fdec64d1e2100eb
SURYAPRATAP SINGH SURYAVANSHI <67123991+suryapratapsinghsuryavanshi@users.noreply.github.com>
2022-07-31 12:37:59 +0000

    add bun install in cron job

--- auto-pull.sh
+++ auto-pull.sh
@@ -8,16 +8,18 @@ state=$(git pull --dry-run 2>&1)
 pm2_state=$(which pm2 2>&1)
 
 # check the state of the repository.
-if [$state == ""]
+if [$state == ""] # check the state of git pull is empty or not.
 then
     echo "Not Want to be update!"
 else
     echo "Update!"
     git pull # Update the repository.
-    if [$pm2_state == ""]
+    if [$pm2_state == ""] # check the state of the pm2 install empty or not.
     then
         echo "pm2 not install!"
-        npm install -g pm2
+        npm install -g pm2 # install pm2
+        bun install # insall all bun dependency
+        export PORT=3000 # assign env port
         pm2 start bun --name "bun_in_production" -- start
     else
         pm2 restart bun_in_production
