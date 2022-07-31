838d4ecad58e60f4c15c716077a6cc0aab0bc88d
SURYAPRATAP SINGH SURYAVANSHI <67123991+suryapratapsinghsuryavanshi@users.noreply.github.com>
2022-07-31 12:29:26 +0000

    add deploy event in auto-pull job

--- auto-pull.sh
+++ auto-pull.sh
@@ -4,6 +4,9 @@
 cd /home/BunJs_In_Production/
 state=$(git pull --dry-run 2>&1)
 
+# get the pm2 installation status.
+pm2_state=$(which pm2 2>&1)
+
 # check the state of the repository.
 if [$state == ""]
 then
@@ -11,6 +14,14 @@ then
 else
     echo "Update!"
     git pull # Update the repository.
+    if [$pm2_state == ""]
+    then
+        echo "pm2 not install!"
+        npm install -g pm2
+        pm2 start bun --name "bun_in_production" -- start
+    else
+        pm2 restart bun_in_production
+    fi
 fi
 
 # We run this shell script by the use cron task.
