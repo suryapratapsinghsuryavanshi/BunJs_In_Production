b03ea1d4cce7376ddbd055c68bdd8d21721ca71c
SURYAPRATAP SINGH SURYAVANSHI <67123991+suryapratapsinghsuryavanshi@users.noreply.github.com>
2022-07-31 12:12:18 +0000

    rectify corn task

--- auto-pull.sh
+++ auto-pull.sh
@@ -2,14 +2,15 @@
 
 # get the status of the repository.
 cd /home/BunJs_In_Production/
-state=$(git pull --dry-run)
+state=$(git pull --dry-run 2>&1)
 
 # check the state of the repository.
-if [$state == ""]; then
+if [$state == ""]
+then
     echo "Not Want to be update!"
 else
     echo "Update!"
-    get pull # Update the repository.
+    git pull # Update the repository.
 fi
 
 # We run this shell script by the use cron task.
